025178c5d0bfdabb1f9274ee4ce51c141295b9a6
khaily <ly.k@husky.neu.edu>
2020-04-08 18:30:06 -0700

    added session (for fun) and mongoose and body parser to server.js

--- server.js
+++ server.js
@@ -1,6 +1,13 @@
+var express = require('express')
+var app = express()
 
-const express = require('express')
-const app = express()
+
+const mongoose = require('mongoose')
+mongoose.connect('mongodb://localhost:27017/cs5610-kl-sp20-A9',
+{ useNewUrlParser: true, useUnifiedTopology: true })
+
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
 
 app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin",
@@ -12,6 +19,13 @@ app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Credentials", "true");
     next();
 });
+
+var session = require('express-session')
+app.use(session({
+    resave: false,
+    saveUninitialized: true,
+    secret: 'any string'
+}));
 
 require('./controllers/quizzes.controller.server')(app)
 require('./controllers/questions.controller.server')(app)
